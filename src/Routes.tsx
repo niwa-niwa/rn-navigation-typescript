65ad3b85a26b10ad075b4e33f2750e751436d54b
niwa-niwa <urban.hang274@gmail.com>
2021-06-27 15:09:00 +0900

    implemented types for route

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,24 +1,72 @@
 import React from "react";
-import { View, Text, } from "react-native";
-import { createStackNavigator } from "@react-navigation/stack";
+import { View, Text, Button } from "react-native";
+import { createStackNavigator, StackNavigationProp } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
+import { Center } from "./Center";
+import { AuthParamList, AuthNavProps } from "./AuthParamList";
 
 interface RoutesProps {}
 
-const Stack = createStackNavigator();
+const Stack = createStackNavigator<AuthParamList>();
 
-function Login(){
-  return(
-    <View>
-      <Text>I am a login screen</Text>
-    </View>
-  )
+function Login({ navigation, route }:AuthNavProps<"Login">) {
+  return (
+    <Center>
+      <Text>route.name:{route.name}</Text>
+      <Button
+        title="go to register"
+        onPress={() => {
+          navigation.navigate("Register");
+        }}
+      />
+    </Center>
+  );
 }
+
+function Register({
+  navigation,
+  route,
+  }:
+    AuthNavProps<"Register"> // same mean
+    // navigation: StackNavigationProp<AuthParamList, 'Register'>;
+    // route: RouteProp<AuthParamList, "Register">;
+  ) {
+  return (
+    <Center>
+      <Text>route:name: {route.name}</Text>
+      <Button
+        title="go to login"
+        onPress={() => {
+          navigation.navigate("Login");
+          // navigation.goBack();
+        }}
+      />
+    </Center>
+  );
+}
+
 export const Routes: React.FC<RoutesProps> = ({}) => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Login" component={Login} />
+      <Stack.Navigator
+        initialRouteName="Login"
+        // screenOptions={{ header: () => null }}
+      >
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{
+            headerTitle:"Sign In"
+          }}
+          />
+        <Stack.Screen
+          name="Register"
+          component={Register}
+          options={{
+            // header: () => null,
+            headerTitle:"Sign Up"
+          }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
