88825d83738cc4d44305482ad16760529a428f39
niwa-niwa <urban.hang274@gmail.com>
2021-06-27 16:05:36 +0900

    implemented authentication with useContext

--- src/AuthParamList.ts
+++ src/AuthParamList.ts
@@ -4,9 +4,9 @@ import { StackNavigationProp } from "@react-navigation/stack";
 export type AuthParamList = {
   Login: undefined;
   Register: undefined;
-}
+};
 
-export type AuthNavProps<T extends keyof AuthParamList>={
+export type AuthNavProps<T extends keyof AuthParamList> = {
   navigation: StackNavigationProp<AuthParamList, T>;
   route: RouteProp<AuthParamList, T>;
-}
+};
--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,18 +1,30 @@
-import React from "react";
-import { View, Text, Button } from "react-native";
-import { createStackNavigator, StackNavigationProp } from "@react-navigation/stack";
+import React, { useState, useEffect, useContext } from "react";
+import { View, Text, Button, ActivityIndicator, AsyncStorage } from "react-native";
+import {
+  createStackNavigator,
+  StackNavigationProp,
+} from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
 import { Center } from "./Center";
 import { AuthParamList, AuthNavProps } from "./AuthParamList";
+import { AuthContext, } from './AuthProvider';
 
 interface RoutesProps {}
 
 const Stack = createStackNavigator<AuthParamList>();
 
-function Login({ navigation, route }:AuthNavProps<"Login">) {
+function Login({ navigation, route }: AuthNavProps<"Login">) {
+  const { login } = useContext(AuthContext);
+
   return (
     <Center>
-      <Text>route.name:{route.name}</Text>
+      <Text>I am a login screen</Text>
+      <Button
+        title="log me in"
+        onPress={()=>{
+          login();
+        }}
+      />
       <Button
         title="go to register"
         onPress={() => {
@@ -23,14 +35,7 @@ function Login({ navigation, route }:AuthNavProps<"Login">) {
   );
 }
 
-function Register({
-  navigation,
-  route,
-  }:
-    AuthNavProps<"Register"> // same mean
-    // navigation: StackNavigationProp<AuthParamList, 'Register'>;
-    // route: RouteProp<AuthParamList, "Register">;
-  ) {
+function Register({ navigation, route }: AuthNavProps<"Register">) {
   return (
     <Center>
       <Text>route:name: {route.name}</Text>
@@ -46,28 +51,62 @@ function Register({
 }
 
 export const Routes: React.FC<RoutesProps> = ({}) => {
+  const { user, login } = useContext(AuthContext);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(()=>{
+    AsyncStorage.getItem("user")
+      .then(userString=>{
+        if(userString){
+          login();
+        }
+        setLoading(false);
+        console.log(userString);
+      })
+      .catch(err => {
+        console.log(err);
+        setLoading(false);
+      });
+  }, []);
+
+  if(loading){
+    return(
+      <Center>
+        <ActivityIndicator size="large" />
+      </Center>
+    )
+  }
+
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Login"
-        // screenOptions={{ header: () => null }}
-      >
-        <Stack.Screen
-          name="Login"
-          component={Login}
-          options={{
-            headerTitle:"Sign In"
-          }}
+      {user ?
+        <Center>
+          <Text>
+            you exist
+          </Text>
+        </Center>
+      :
+        <Stack.Navigator
+          initialRouteName="Login"
+          // screenOptions={{ header: () => null }}
+        >
+          <Stack.Screen
+            name="Login"
+            component={Login}
+            options={{
+              headerTitle: "Sign In",
+            }}
           />
-        <Stack.Screen
-          name="Register"
-          component={Register}
-          options={{
-            // header: () => null,
-            headerTitle:"Sign Up"
-          }}
-        />
-      </Stack.Navigator>
+          <Stack.Screen
+            name="Register"
+            component={Register}
+            options={{
+              // header: () => null,
+              headerTitle: "Sign Up",
+            }}
+          />
+        </Stack.Navigator>
+      }
     </NavigationContainer>
   );
 };
